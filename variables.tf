bb8610dcbfaa7b807b3df2de8564b8462f80f192
Sergey Pashkov <serpashk@gmail.com>
2024-01-06 13:35:52 +0000

    added tf files

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "GITHUB_OWNER" {
+  type = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+    type = string
+    description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+    type = string
+    default = "flux-gitops"
+    description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type = string
+  default = "clusters"
+  description = "Flux manifests subdirectory"
+}
