dbd7313f2644a849cc70c389be63ef602f87eabd
Sergey Pashkov <serpashk@gmail.com>
2024-01-11 17:58:25 +0000

    Updated Terraform files for complete automated infrastructure creation and app deployment

--- main.tf
+++ main.tf
@@ -1,25 +1,66 @@
-module "github_repository" {
-    source = "github.com/den-vasyliev/tf-github-repository"
-    github_owner = var.GITHUB_OWNER
-    github_token = var.GITHUB_TOKEN
-    repository_name = var.FLUX_GITHUB_REPO
-    public_key_openssh = module.tls_private_key.public_key_openssh
-    public_key_openssh_title = "flux0"
+terraform {
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = "1.0.0-rc.3"
+    }
+  }
 }
 
+# GitHub infrastructure repository
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+
+  # Add configs to repository to automatically run PET project in Flux
+  configs_path_local       = "clusters/demo"
+  configs_path_remote      = "${var.FLUX_GITHUB_TARGET_PATH}/demo"
+}
+
+# Use kubernetes in docker for local testing
 module "kind_cluster" {
   source = "github.com/sergeypashkov/tf-kind-cluster"
 }
 
-module "flux_bootstrap" {
-    source = "github.com/sergeypashkov/tf-fluxcd-flux-bootstrap"
-    github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-    private_key = module.tls_private_key.private_key_pem
-    config_path = module.kind_cluster.kubeconfig
-}
+# GKE cluster for cloud deployment.
+# Note, here we pass the machine type, the default is set to n1-standard-4
+# because deployment with less powerful configuration hangs and fails with timeout.
+#  module "gke_cluster" {
+#  source = "github.com/sergeypashkov/tf-google-gke-cluster"
+#  GOOGLE_REGION = var.GOOGLE_REGION
+#  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+#  GKE_NUM_NODES = 1
+#  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
+# }
 
+# Generate SSH keys
 module "tls_private_key" {
-  source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+  source    = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
   algorithm = "RSA"
 }
+
+# Use Flux provider directly
+provider "flux" {
+  kubernetes = {
+    config_path = module.kind_cluster.kubeconfig
+  }
+  git = {
+    url = "ssh://github.com/${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}.git"
+    ssh = {
+      username    = "git"
+      private_key = module.tls_private_key.private_key_pem
+    }
+  }
+}
+
+# It depends on repository and its SSH key, so wait for creation 
+resource "flux_bootstrap_git" "this" {
+  depends_on = [module.github_repository]
+  path       = var.FLUX_GITHUB_TARGET_PATH
+}
+
 
--- variables.tf
+++ variables.tf
@@ -1,21 +1,39 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  default     = ""
+  description = "GCP project to use"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "n1-standard-4"
+  description = "GKE machine type"
+}
+
 variable "GITHUB_OWNER" {
-  type = string
+  type        = string
   description = "GitHub owner repository to use"
 }
 
 variable "GITHUB_TOKEN" {
-    type = string
-    description = "GitHub personal access token"
+  type        = string
+  description = "GitHub personal access token"
 }
 
 variable "FLUX_GITHUB_REPO" {
-    type = string
-    default = "flux-gitops"
-    description = "Flux GitOps repository"
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
 }
 
 variable "FLUX_GITHUB_TARGET_PATH" {
-  type = string
-  default = "clusters"
+  type        = string
+  default     = "clusters"
   description = "Flux manifests subdirectory"
 }
